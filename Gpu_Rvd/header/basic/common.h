ea8f15992ae31211226a22923684649d168e0218
JWhan <stormhan1205@gmail.com>
2016-11-24 23:00:39 +0800

    add common lib

---
+++ Gpu_Rvd/header/basic/common.h
@@ -0,0 +1,25 @@
+/*
+	do the prework
+*/
+
+#ifndef BASIC_COMMON_H
+#define BASIC_COMMON_H
+
+/*
+	this header should be included before anything else
+*/
+#include <iostream>
+
+/*
+	include the STL container so that we can be more comfortable 
+	when we handle something like the file.
+*/
+#include <vector>
+#include <string>
+
+namespace Gpu_Rvd{
+
+	typedef unsigned int index_t;
+
+}
+#endif /* BASIC_COMMON_H */
