1c12aac2240dd8c27d41acfcdc88fffd4b811509
JWhan <stormhan1205@gmail.com>
2016-11-24 23:01:37 +0800

    add numeric

---
+++ Gpu_Rvd/header/basic/numeric.h
@@ -0,0 +1,13 @@
+/*
+	handle basic operation about number.
+*/
+
+#ifndef BASIC_NUMERIC_H
+#define BASIC_NUMERIC_H
+
+#include <basic\common.h>
+
+// indice of loop
+typedef unsigned int index_t
+
+#endif /* BASIC_NUMERIC_H */
