e45f3bde3b476a125785cb79f416b337b2d3e478
JWhan <stormhan1205@gmail.com>
2016-11-26 18:52:18 +0800

    add typedef int, uint, void*

--- Gpu_Rvd/header/basic/numeric.h
+++ Gpu_Rvd/header/basic/numeric.h
@@ -7,7 +7,44 @@
 
 #include <basic\common.h>
 
+#include <stdint.h>
 // indice of loop
-typedef unsigned int index_t
+//typedef unsigned int index_t
 
+namespace Gpu_Rvd{
+
+	/** Generic pointer type */
+	typedef void* pointer;
+
+	/** Integer type with a width of 8 bits */
+	typedef int8_t int8;
+
+	/** Integer type with a width of 16 bits */
+	typedef int16_t int16;
+
+	/** Integer type with a width of 32 bits */
+	typedef int32_t int32;
+
+	/** Integer type with a width of 64 bits */
+	typedef int64_t int64;
+
+	/** Unsigned integer type with a width of 8 bits */
+	typedef uint8_t uint8;
+
+	/** Unsigned integer type with a width of 16 bits */
+	typedef uint16_t uint16;
+
+	/** Unsigned integer type with a width of 32 bits */
+	typedef uint32_t uint32;
+
+	/** Unsigned integer type with a width of 64 bits */
+	typedef uint64_t uint64;
+
+	/** Floating point type with a width of 32 bits */
+	typedef float float32;
+
+	/** Floating point type with a width of 64 bits */
+	typedef double float64;
+
+}
 #endif /* BASIC_NUMERIC_H */
