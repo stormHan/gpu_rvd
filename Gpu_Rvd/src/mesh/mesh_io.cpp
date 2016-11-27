471d40cf13c9f130e05509e067f3699016b59146
JWhan <stormhan1205@gmail.com>
2016-11-27 23:36:50 +0800

    add mesh_io

--- Gpu_Rvd/src/mesh/mesh_io.cpp
+++ Gpu_Rvd/src/mesh/mesh_io.cpp
@@ -3,7 +3,7 @@
 
 namespace Gpu_Rvd{
 
-	bool mesh_load(const std::string filename, Mesh& M, FileType filetype){
+	bool mesh_load_obj(const std::string filename, Mesh& M){
 		coords_index_t dim = M.dimension();
 		std::vector<double> P(dim);
 
@@ -54,6 +54,35 @@ namespace Gpu_Rvd{
 						facet_vertices.push_back(vertex_index - 1);
 					}
 					M.add_facet(facet_vertices);
+				}
+			}
+		}
+		return true;
+	}
+
+	bool points_load_obj(const std::string filename, Points& points){
+		coords_index_t dim = points.dimension();
+		std::vector<double> P(dim);
+
+		LineInput in(filename);
+		if (!in.OK()){
+			return false;
+		}
+		bool first_facet_attribute = true;
+		bool read_facet_regions = false;
+		while (!in.eof() && in.get_line()){
+			in.get_fields(); //must be called after get_line()
+			if (in.nb_fields() >= 1){
+				if (in.field_matches(0, "v")){
+					for (coords_index_t c = 0; c < dim; ++c){
+						if (index_t(c + 1) < in.nb_fields()){
+							P[c] = in.field_as_double(index_t(c + 1));
+						}
+						else{
+							P[c] = 0.0;
+						}
+					}
+					points.add_vertexd(P);
 				}
 			}
 		}
