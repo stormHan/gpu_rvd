1b7769c31a950a4df2ec1d06b9b29f39ead44d90
JWhan <stormhan1205@gmail.com>
2016-11-27 23:36:27 +0800

    add comom

--- Gpu_Rvd/header/basic/common.h
+++ Gpu_Rvd/header/basic/common.h
@@ -19,7 +19,16 @@
 
 namespace Gpu_Rvd{
 
+	/* Represents the non-negative indices */
 	typedef unsigned int index_t;
+
+	/* Represents the possibly negative indices */
+	typedef int signed_index_t;
+
+	/* Represents the small size coords indices */
+	typedef unsigned char coords_index_t;
+
+#define nil 0
 
 }
 #endif /* BASIC_COMMON_H */
