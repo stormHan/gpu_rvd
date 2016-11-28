72e6c8d2fe0038a413e0ddf4c1acffd7c9bbf039
JWhan <stormhan1205@gmail.com>
2016-11-28 22:23:50 +0800

    modify mesh.h

--- Gpu_Rvd/header/mesh/mesh.h
+++ Gpu_Rvd/header/mesh/mesh.h
@@ -8,6 +8,7 @@
 #define MESH_MESH_H
 
 #include <basic\common.h>
+#include <mesh\mesh_nn.h>
 
 namespace Gpu_Rvd{
 	
@@ -15,19 +16,77 @@ namespace Gpu_Rvd{
 	public:
 		Points();
 
-		const index_t dimension() const{ return dimension_; }
+		/*
+		 * \brief Gets the dimension fo the Points.
+		 */
+		const index_t dimension() const{
+			return dimension_; 
+		}
 
+		/*
+		 * \brief Checks if the data is single precision.
+		 */
 		bool is_single_precision();
 
+		/*
+		* \brief Checks if the data is double precision.
+		*/
 		bool is_double_precision();
-
+		
+		/*
+		* \brief Get the vertex(double) with the indice.
+		*/
 		const double* get_vertexd(index_t t) const;
 
+		/*
+		* \brief Adds a vertex(double) which data is in pointer.
+		*/
 		void add_vertexd(const double* p, index_t dim);
 
+		/*
+		* \brief Adds a vertex(double) which data is std::vector<double>.
+		*/
 		void add_vertexd(const std::vector<double>& p);
 
+		/*
+		 * \breif Gets the vertex number.
+		 */
 		const index_t get_vertex_nb() const{ return vertex_nb_; }
+
+		/*
+		 * \brief Gets the beginning pointer of the vertex.
+		 */
+		const double* v_ptr(){
+			return &vertexd_[0];
+		}
+
+		/*
+		 * \breif Initializes of the Points_nn.
+		 */
+		void initialize_points_nn(index_t k, coords_index_t dim, bool selfstore);
+
+		/*
+		 * \brief Searches the nearest neighbors
+		 * \detials \param[double* query]	the query data.
+		 *			\param[dim]				indicates the stride of the data of one single point.
+		 *			\param[search_nb] 		indicates how many points to be searched.
+		 *			\param[ind]				index_t type pointer to store the search result.
+		 *			\param[dist]			double type pointer to store the distance2 result.
+		 */
+		void search_nn(const double* query, coords_index_t dim, index_t search_nb, index_t* ind, double* dist, index_t k);
+
+		/**
+		 * \brief Searches the nearest neighbors in self-store mode.
+		 */
+		void search_nn(const double* query, coords_index_t dim, index_t search_nb, index_t k);
+
+
+		/*
+		 * \brief Gets the points_nn's indices
+		 */
+		const index_t* get_indices() const{
+			return points_nn_.get_index();
+		}
 
 	protected:
 		std::vector<double>		vertexd_;
@@ -37,6 +96,9 @@ namespace Gpu_Rvd{
 		const static index_t	dimension_ = 3;
 		bool					is_single_precision_;
 		bool					is_double_precision_;
+
+		Points_nn				points_nn_;
+
 	};
 
 
