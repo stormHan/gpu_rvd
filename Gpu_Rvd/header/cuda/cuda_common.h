144d38fd8205da2fbd270cee9b792e629e69b246
JWhan <stormhan1205@gmail.com>
2016-11-30 19:43:39 +0800

    add several #define

---
+++ Gpu_Rvd/header/cuda/cuda_common.h
@@ -0,0 +1,19 @@
+/*
+ * \header Cuda Common.
+ */
+
+#ifndef CUDA_COMMON_H
+#define CUDA_COMMON_H
+
+#include <cuda_runtime.h>
+
+
+namespace Gpu_Rvd{
+
+#define DOUBLE_SIZE sizeof(double)
+#define FLOAT_SIZE sizeof(double)
+#define INT_SIZE sizeof(int)
+
+}
+
+#endif /* CUDA_COMMON_H */
