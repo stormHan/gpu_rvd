2c60702719f59d96651a92f11c09e6d03c89948b
JWhan <stormhan1205@gmail.com>
2016-12-01 16:58:58 +0800

    cuda polygon

---
+++ Gpu_Rvd/header/cuda/cuda_polygon.h
@@ -0,0 +1,36 @@
+/*
+ *
+ */
+
+#ifndef CUDA_POLYGON_H
+#define CUDA_POLYGON_H
+
+namespace Gpu_Rvd{
+
+	/*
+	* \brief Cuda polygon vertex.
+	* \detials x, y, z is the spatial position of a vertxt.
+	*	w is the weight of a vextex. neigh_s is the nearby seed
+	* of the current point.
+	*/
+	struct CudaVertex
+	{
+		double x;
+		double y;
+		double z;
+		double w;
+
+		int neigh_s = -1;
+	};
+
+	/*
+	* \brief Cuda polygon. A smart data stuction to store the clipped triangle.
+	*/
+	struct CudaPolygon
+	{
+		CudaVertex vertex[10];
+		index_t vertex_nb;
+	};
+}
+
+#endif /* CUDA_POLYGON_H */
