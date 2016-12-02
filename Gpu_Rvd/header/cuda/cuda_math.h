5633bb1f646fd96b35fb96cbb27cd0dd1eaf1c75
JWhan <stormhan1205@gmail.com>
2016-12-02 15:07:29 +0800

    add inline math lib

--- Gpu_Rvd/header/cuda/cuda_math.h
+++ Gpu_Rvd/header/cuda/cuda_math.h
@@ -5,6 +5,8 @@
 #ifndef CUDA_MATH_H
 #define CUDA_MATH_H
 
+#include <cuda\cuda_common.h>
+
 namespace Gpu_Rvd{
 
 	__device__
@@ -12,6 +14,11 @@ namespace Gpu_Rvd{
 		return (p2.x - p1.x) * (p2.x - p1.x)
 			+ (p2.y - p1.y) * (p2.y - p1.y)
 			+ (p2.z - p1.z) * (p2.z - p1.z);
+	}
+
+	__device__
+	inline double distance(double3 p1, double3 p2){
+		return sqrt(distance2(p1, p2));
 	}
 
 	__device__
@@ -59,6 +66,41 @@ namespace Gpu_Rvd{
 	{
 		if (x > 0) return x;
 		else return -x;
+	}
+
+	__device__
+	inline double computeTriangleArea(double3 p1, double3 p2, double3 p3)
+	{
+		double a = distance(p1, p2);
+		double b = distance(p2, p3);
+		double c = distance(p1, p3);
+
+		//Heron's Formula to compute the area of the triangle
+		double p = (a + b + c) / 2;
+		if (p >= a && p >= b && p >= c)
+			return sqrt(p * (p - a) * (p - b) * (p - c));
+		else
+			return 0.0;
+	}
+
+	__device__
+	inline void computeTriangleCentriod(
+		double3 p, double3 q, double3 r, double a, double b, double c,
+		double3& Vg, double& V
+		)
+	{
+		double abc = a + b + c;
+		double area = computeTriangleArea(p, q, r);
+		V = area / 3.0 * abc;
+
+		double wp = a + abc;
+		double wq = b + abc;
+		double wr = c + abc;
+
+		double s = area / 12.0;
+		Vg.x = s * (wp * p.x + wq * q.x + wr * r.x);
+		Vg.y = s * (wp * p.y + wq * q.y + wr * r.y);
+		Vg.z = s * (wp * p.z + wq * q.z + wr * r.z);
 	}
 }
 
