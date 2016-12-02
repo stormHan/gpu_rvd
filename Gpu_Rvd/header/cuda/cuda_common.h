4e4439041df0ab22d4dfac6e9d6f2e7428b2897a
JWhan <stormhan1205@gmail.com>
2016-12-02 15:08:04 +0800

    add global variables

--- Gpu_Rvd/header/cuda/cuda_common.h
+++ Gpu_Rvd/header/cuda/cuda_common.h
@@ -15,6 +15,17 @@ namespace Gpu_Rvd{
 #define INT_SIZE sizeof(int)
 
 	const index_t CUDA_Stack_size = 10;
+
+	//Set cuda global variables.
+	//Adds the RVD information to seeds_information to iterate 
+	//the points' position.
+	__device__
+		double* g_seeds_information;
+
+	//Records how many polygons belonged to each point.
+	__device__
+		int*	g_seeds_polygon_nb;
+	
 }
 
 #endif /* CUDA_COMMON_H */
