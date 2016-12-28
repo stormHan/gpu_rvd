c2784480f40e9c0f11f9491c04257218eada970a
JWhan <stormhan1205@gmail.com>
2016-12-28 22:40:38 +0800

    add random triangle pointing function

--- Gpu_Rvd/header/basic/math_op.h
+++ Gpu_Rvd/header/basic/math_op.h
@@ -8,7 +8,7 @@
 #define H_BASIC_COMMON
 
 #include <basic\common.h>
-
+#include <basic\numeric.h>
 #include <math.h>
 
 namespace Gpu_Rvd{
@@ -32,6 +32,26 @@ namespace Gpu_Rvd{
 				result[t] = (float)((p1[t] + p2[t] + p3[t]) / 3);
 			}
 		}
+
+		template <class T>
+		inline T tri_area(const T* p1, const T* p2, const T* p3, index_t dim){
+			vec3g<T> p(p1), q(p2), r(p3);
+			return length(cross(q - p, r - p)) / T(2);
+		}
+
+		template <class T>
+		inline vec3g<T> random_point_tri(const vec3g<T>& p1, const vec3g<T>& p2, const vec3g<T>& p3){
+			Real s = Numeric::random_float64();
+			Real t = Numeric::random_float64();
+
+			if (s + t > 1){
+				s = 1 - s;
+				t = 1 - t;
+			}
+			return (1 - s - t)*p1 + s*p2 + t*p3;
+		}
+		
+
 	}
 
 }
