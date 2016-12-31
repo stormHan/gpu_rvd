3feb2a615cc3b863c41a7f9c7886cbb5160a01f4
JWhan <stormhan1205@gmail.com>
2016-12-31 21:36:37 +0800

    complete kdtree class

--- Gpu_Rvd/header/points/kd_tree.cpp
+++ Gpu_Rvd/header/points/kd_tree.cpp
@@ -1,5 +1,51 @@
 
 #include <points\kd_tree.h>
+#include <basic\math_op.h>
+
+namespace {
+	using namespace Gpu_Rvd;
+
+	/**
+	 * \brief Comparision functor used to
+	 * sort the point indices.
+	 */
+	class ComparePointCoord{
+	public :
+		/**
+		* \brief Creates a new ComparePointCoord
+		* \param[in] nb_points number of points
+		* \param[in] points pointer to first point
+		* \param[in] stride number of doubles between two
+		*  consecutive points in array (=dimension if point
+		*  array is compact).
+		* \param[in] splitting_coord the coordinate to compare
+		*/
+		ComparePointCoord(
+			index_t nb_points,
+			const double* points,
+			index_t stride,
+			coords_index_t splitting_coord
+			) :
+			nb_points_(nb_points),
+			points_(points),
+			stride_(stride),
+			splitting_coord_(splitting_coord) {
+		}
+
+		bool operator() (index_t i, index_t j) const{
+			return
+				(points_ + i * stride_)[splitting_coord_] <
+				(points_ + j * stride_)[splitting_coord_];
+		}
+
+	private:
+		index_t nb_points_;
+		const double* points_;
+		index_t stride_;
+		coords_index_t splitting_coord_;
+	};
+
+}
 
 namespace Gpu_Rvd{
 
@@ -24,5 +70,288 @@ namespace Gpu_Rvd{
 		index_t nb_points, const double* points
 		){
 		set_points(nb_points, points, dimension());
+	}
+
+	void Kdtree::set_points(index_t nb_points, const double* points,
+		index_t stride){
+		nb_points_ = nb_points;
+		points_ = points;
+		stride_ = stride;
+
+		index_t sz = max_node_index(1, 0, nb_points) + 1;
+
+		point_index_.resize(nb_points);
+		splitting_coord_.resize(sz);
+		splitting_val_.resize(sz);
+
+		for (index_t i = 0; i < nb_points; i++) {
+			point_index_[i] = i;
+		}
+
+		//Parallel
+		//...
+
+		create_kd_tree_recursive(1, 0, nb_points);
+
+		// Compute the bounding box.
+		for (coords_index_t c = 0; c < dimension(); ++c) {
+			bbox_min_[c] = 1e30;
+			bbox_max_[c] = -1e30;
+		}
+		for (index_t i = 0; i < nb_points; ++i) {
+			const double* p = point_ptr(i);
+			for (coords_index_t c = 0; c < dimension(); ++c) {
+				bbox_min_[c] = geo_min(bbox_min_[c], p[c]);
+				bbox_max_[c] = geo_max(bbox_max_[c], p[c]);
+			}
+		}
+	}
+
+	coords_index_t Kdtree::best_splitting_coord(
+		index_t b, index_t e
+		){
+		// Returns the coordinates that maximizes
+		// point's spread. We should probably
+		// use a tradeoff between spread and
+		// bbox shape ratio, as done in ANN, but
+		// this simple method seems to give good
+		// results in our case.
+		coords_index_t result = 0;
+		double max_spread = spread(b, e, 0);
+		for (coords_index_t c = 1; c < dimension(); ++c){
+			double coord_spread = spread(b, e, c);
+			if (coord_spread > max_spread){
+				result = c;
+				max_spread = coord_spread;
+			}
+		}
+		return result;
+	}
+
+	void Kdtree::operator() (index_t i) {
+		switch (i) {
+			// Second level of the tree: create two nodes in
+			//  parallel.
+		case 2:
+			m2_ = split_kd_node(2, m0_, m4_);
+			break;
+		case 3:
+			m6_ = split_kd_node(3, m4_, m8_);
+			break;
+
+			// Third level of the tree: create four nodes in
+			// parallel.
+		case 4:
+			m1_ = split_kd_node(4, m0_, m2_);
+			break;
+		case 5:
+			m3_ = split_kd_node(5, m2_, m4_);
+			break;
+		case 6:
+			m5_ = split_kd_node(6, m4_, m6_);
+			break;
+		case 7:
+			m7_ = split_kd_node(7, m6_, m8_);
+			break;
+
+			// Fourth level of the tree: create eight subtrees
+			// in parallel.
+		case 8:
+			create_kd_tree_recursive(8, m0_, m1_);
+			break;
+		case 9:
+			create_kd_tree_recursive(9, m1_, m2_);
+			break;
+		case 10:
+			create_kd_tree_recursive(10, m2_, m3_);
+			break;
+		case 11:
+			create_kd_tree_recursive(11, m3_, m4_);
+			break;
+		case 12:
+			create_kd_tree_recursive(12, m4_, m5_);
+			break;
+		case 13:
+			create_kd_tree_recursive(13, m5_, m6_);
+			break;
+		case 14:
+			create_kd_tree_recursive(14, m6_, m7_);
+			break;
+		case 15:
+			create_kd_tree_recursive(15, m7_, m8_);
+			break;
+		}
+	}
+
+	index_t Kdtree::split_kd_node(
+		index_t node_index, index_t b, index_t e
+		){
+		//Do not split leafs
+		if (b + 1 == e){
+			return b;
+		}
+		coords_index_t splitting_coord = best_splitting_coord(b, e);
+		index_t m = b + (e - b) / 2;
+
+		std::nth_element(
+			point_index_.begin() + std::ptrdiff_t(b),
+			point_index_.begin() + std::ptrdiff_t(m),
+			point_index_.begin() + std::ptrdiff_t(e),
+			ComparePointCoord(
+			nb_points_, points_, stride_, splitting_coord
+			)
+			);
+
+		// Initialize node's variables (splitting coord and
+		// splitting value)
+		splitting_coord_[node_index] = splitting_coord;
+		splitting_val_[node_index] =
+			point_ptr(point_index_[m])[splitting_coord];
+		return m;
+	}
+
+	void Kdtree::get_nearest_neighbors(
+		index_t nb_neighbors,
+		const double* query_point,
+		index_t* neighbors,
+		double* neighbors_sq_dist
+		) const {
+
+		// Compute distance between query point and global bounding box
+		// and copy global bounding box to local variables (bbox_min, bbox_max),
+		// allocated on the stack. bbox_min and bbox_max are updated during the
+		// traversal of the KdTree (see get_nearest_neighbors_recursive()). They
+		// are necessary to compute the distance between the query point and the
+		// bbox of the current node.
+		double box_dist = 0.0;
+		double* bbox_min = (double*)(alloca(dimension() * sizeof(double)));
+		double* bbox_max = (double*)(alloca(dimension() * sizeof(double)));
+		for (coords_index_t c = 0; c < dimension(); ++c) {
+			bbox_min[c] = bbox_min_[c];
+			bbox_max[c] = bbox_max_[c];
+			if (query_point[c] < bbox_min_[c]) {
+				box_dist += geo_sqr(bbox_min_[c] - query_point[c]);
+			}
+			else if (query_point[c] > bbox_max_[c]) {
+				box_dist += geo_sqr(bbox_max_[c] - query_point[c]);
+			}
+		}
+		NearestNeighbors NN(
+			nb_neighbors, neighbors, neighbors_sq_dist
+			);
+		get_nearest_neighbors_recursive(
+			1, 0, nb_points(), bbox_min, bbox_max, box_dist, query_point, NN
+			);
+	}
+
+
+	void Kdtree::get_nearest_neighbors(
+		index_t nb_neighbors,
+		index_t q_index,
+		index_t* neighbors,
+		double* neighbors_sq_dist
+		) const {
+		// TODO: optimized version that uses the fact that
+		// we know that query_point is in the search data
+		// structure already.
+		// (I tryed something already, see in the Attic, 
+		//  but it did not give any significant speedup).
+		get_nearest_neighbors(
+			nb_neighbors, point_ptr(q_index),
+			neighbors, neighbors_sq_dist
+			);
+	}
+
+	void Kdtree::get_nearest_neighbors_recursive(
+		index_t node_index, index_t b, index_t e,
+		double* bbox_min, double* bbox_max,
+		double box_dist, const double* query_point,
+		NearestNeighbors& NN
+		) const{
+		
+		//Simple case (node is a leaf)
+		if ((e - b) <= MAX_LEAF_SIZE){
+			for (index_t i = b; i < e; ++i){
+				index_t p = point_index_[i];
+				double d2 = Math::distance2(
+					query_point, point_ptr(p), dimension()
+					);
+				NN.insert(p, d2);
+			}
+			return;
+		}
+		
+		coords_index_t coord = splitting_coord_[node_index];
+		double val = splitting_val_[node_index];
+		double cut_diff = query_point[coord] - val;
+		index_t m = b + (e - b) / 2;
+
+		//If the query point is on the left side
+		if (cut_diff < 0.0){
+			// Traverse left subtree
+			{
+				double bbox_max_save = bbox_max[coord];
+				bbox_max[coord] = val;
+				get_nearest_neighbors_recursive(
+					2 * node_index, b, m,
+					bbox_min, bbox_max, box_dist, query_point, NN
+					);
+				bbox_max[coord] = bbox_max_save;
+
+				// Update bbox distance (now measures the
+				// distance to the bbox of the right subtree)
+				double box_diff = bbox_min[coord] - query_point[coord];
+				if (box_diff > 0.0) {
+					box_dist -= geo_sqr(box_diff);
+				}
+				box_dist += geo_sqr(cut_diff);
+			}
+			// Traverse the right subtree, only if bbox
+			// distance is nearer than furthest neighbor,
+			// else there is no chance that the right
+			// subtree contains points that will change
+			// anything in the nearest neighbors NN.
+			if (box_dist <= NN.furthest_neighbor_sq_dist()) {
+				double bbox_min_save = bbox_min[coord];
+				bbox_min[coord] = val;
+				get_nearest_neighbors_recursive(
+					2 * node_index + 1, m, e,
+					bbox_min, bbox_max, box_dist, query_point, NN
+					);
+				bbox_min[coord] = bbox_min_save;
+			}
+		}
+		else{
+			// else the query point is on the right side
+			// (then do the same with left and right subtree
+			//  permutted).
+			{
+				double bbox_min_save = bbox_min[coord];
+				bbox_min[coord] = val;
+				get_nearest_neighbors_recursive(
+					2 * node_index + 1, m, e,
+					bbox_min, bbox_max, box_dist, query_point, NN
+					);
+				bbox_min[coord] = bbox_min_save;
+			}
+
+			// Update bbox distance (now measures the
+			// distance to the bbox of the left subtree)
+			double box_diff = query_point[coord] - bbox_max[coord];
+			if (box_diff > 0.0) {
+				box_dist -= geo_sqr(box_diff);
+			}
+			box_dist += geo_sqr(cut_diff);
+
+			if (box_dist <= NN.furthest_neighbor_sq_dist()) {
+				double bbox_max_save = bbox_max[coord];
+				bbox_max[coord] = val;
+				get_nearest_neighbors_recursive(
+					2 * node_index, b, m,
+					bbox_min, bbox_max, box_dist, query_point, NN
+					);
+				bbox_max[coord] = bbox_max_save;
+			}
+		}
 	}
 }
--- Gpu_Rvd/header/points/kd_tree.h
+++ Gpu_Rvd/header/points/kd_tree.h
@@ -19,17 +19,35 @@ namespace Gpu_Rvd{
 
 		virtual void set_points(index_t nb_points, const double* points);
 
-		void get_nearest_neighbors(
-			index_t nb_neighbors,
-			const double* query_point,
-			index_t* neighbors,
-			double* neighbors_sq_dist
-			) const {}
-
+		virtual bool stride_supported() const { return true; }
+		
 		virtual void set_points(
 			index_t nb_points, const double* points, index_t stride
 			);
 
+		virtual void get_nearest_neighbors(
+			index_t nb_neighbors,
+			const double* query_point,
+			index_t* neighbors,
+			double* neighbors_sq_dist
+			) const;
+
+		virtual void get_nearest_neighbors(
+			index_t nb_neighbors,
+			index_t query_point,
+			index_t* neighbors,
+			double* neighbors_sq_dist
+			) const;
+
+		
+	public:
+		/**
+		* \brief Used by multithread tree construction
+		* in the implementation of set_points()
+		*/
+		void operator() (index_t t);
+
+		//virtual ~Kdtree();
 	protected:
 		virtual ~Kdtree();
 
@@ -38,6 +56,192 @@ namespace Gpu_Rvd{
 		 */
 		static const index_t MAX_LEAF_SIZE = 16;
 
+		/*
+		 * \brief Returns the maximum node index in subtree
+		 * \param[in] node_id node index of the subtree
+		 * \param[in] b first index of the points sequence in subtree.
+		 * \param[in] e one position past the last index of the points
+		 * sequence in the subtree.
+		 */
+		static index_t max_node_index(
+			index_t node_id, index_t b, index_t e
+			){
+			//when recursive to leaf node, return 
+			//the index of node_id
+			if (e - b <= MAX_LEAF_SIZE){
+				return node_id;
+			}
+
+			index_t m = b + (e - b) / 2;
+			return geo_max(
+				max_node_index(node_id * 2, b, m),
+				max_node_index(node_id * 2 + 1, m, e)
+				);
+		}
+
+		/**
+		* \brief The context for traversing a KdTree.
+		* \details Stores a sorted sequence of (point,distance)
+		*  couples.
+		*/
+		struct NearestNeighbors{
+
+			/**
+			* \brief Creates a new NearestNeighbors
+			* \details Storage is provided
+			* and managed by the caller.
+			* Initializes neighbors_sq_dist[0..nb_neigh-1]
+			* to Numeric::max_float64() and neighbors[0..nb_neigh-1]
+			* to index_t(-1).
+			* \param[in] nb_neighbors_in number of neighbors to retreive
+			* \param[in] neighbors_in storage for the neighbors, allocated
+			*  and managed by caller
+			* \param[in] neighbors_sq_dist_in storage for neighbors squared
+			*  distance, allocated and managed by caller
+			*/
+			NearestNeighbors(
+				index_t nb_neighbors_in,
+				index_t* neighbors_in,
+				double* neighbors_sq_dist_in
+				) :
+				nb_neighbors(nb_neighbors_in),
+				neighbors(neighbors_in),
+				neighbors_sq_dist(neighbors_sq_dist_in)
+			{
+				for (index_t t = 0; t < nb_neighbors; ++t){
+					neighbors[t] = index_t(-1);
+					neighbors_sq_dist[t] = Numeric::max_float64();
+				}
+			}
+
+			/**
+			 * \brief Inserts a new neighbor.
+			 * \details Only the nb_neighbor nearest points are kept.
+			 * \param[in] neighbor the index of the point.
+			 * \param[in] sq_dist the squared distance between the point
+			 * and the query point.
+			 */
+			void insert(
+				index_t neighbor, double sq_dist
+				){
+				if (sq_dist >= furthest_neighbor_sq_dist())
+					return;
+				index_t i = nb_neighbors;
+				while (i != 0 && neighbors_sq_dist[i - 1] > sq_dist){
+					if (i < nb_neighbors){
+						neighbors[i] = neighbors[i - 1];
+						neighbors_sq_dist[i] = neighbors_sq_dist[i - 1];
+					}
+					i--;
+				}
+				neighbors[i] = neighbor;
+				neighbors_sq_dist[i] = sq_dist;
+			}
+
+			/**
+			 * \brief Gets the squared distance to the furthest neighbor.
+			 */
+			double furthest_neighbor_sq_dist() const{
+				return neighbors_sq_dist[nb_neighbors - 1];
+			}
+
+			index_t nb_neighbors;
+			index_t* neighbors;
+			double* neighbors_sq_dist;
+		};
+
+		/**
+		 * \brief Computes the coordinate along which a point
+		 *	sequence will be splitted
+		 */
+		coords_index_t best_splitting_coord(index_t b, index_t e);
+
+		/**
+		* \brief Computes the extent of a point sequence along a coordinate.
+		* \param[in] b first index of the point sequence
+		* \param[in] e one position past the last index of the point sequence
+		* \param[in] coord coordinate along which the extent is measured
+		*/
+		double spread(
+			index_t b, index_t e, coords_index_t coord
+			) {
+			double minval = Numeric::max_float64();
+			double maxval = Numeric::min_float64();
+			for (index_t i = b; i < e; ++i) {
+				double val = point_ptr(point_index_[i])[coord];
+				minval = geo_min(minval, val);
+				maxval = geo_max(maxval, val);
+			}
+			return maxval - minval;
+		}
+
+		/**
+		* \brief Creates the subtree under a node.
+		* \param[in] node_index index of the node that represents
+		*  the subtree to create
+		* \param[in] b first index of the point sequence in the subtree
+		* \param[in] e one position past the last index of the point
+		*  index in the subtree
+		*/
+		void create_kd_tree_recursive(
+			index_t node_index, index_t b, index_t e
+			){
+			if (e - b <= MAX_LEAF_SIZE){
+				return;
+			}
+			index_t m = split_kd_node(node_index, b, e);
+			create_kd_tree_recursive(node_index * 2, b, m);
+			create_kd_tree_recursive(node_index * 2 + 1, m, e);
+		}
+
+		/**
+		 * \brief Computes and stores the splitting coordinate
+		 * and splitting value of the node node_index, that
+		 * correspponds to the [b, e) points sequence.
+		 *
+		 * \return a node index m. The point sequence [b, m)
+		 * and [m, e)correspond to the left child(2*node_index)
+		 * and right child (2*node_index+1) of node_index
+		 */
+		index_t split_kd_node(
+			index_t node_index, index_t b, index_t e
+			);
+
+		/**
+		* \brief The recursive function to implement KdTree traversal and
+		*  nearest neighbors computation.
+		* \details Traverses the subtree under the
+		*  node_index node that corresponds to the
+		*  [b,e) point sequence. Nearest neighbors
+		*  are inserted into neighbors during
+		*  traversal.
+		* \param[in] node_index index of the current node in the Kd tree
+		* \param[in] b index of the first point in the subtree under
+		*  node \p node_index
+		* \param[in] e one position past the index of the last point in the
+		*  subtree under node \p node_index
+		* \param[in,out] bbox_min coordinates of the lower
+		*  corner of the bounding box.
+		*  Allocated and managed by caller.
+		*  Modified by the function and restored on exit.
+		* \param[in,out] bbox_max coordinates of the
+		*  upper corner of the bounding box.
+		*  Allocated and managed by caller.
+		*  Modified by the function and restored on exit.
+		* \param[in] bbox_dist squared distance between
+		*  the query point and a bounding box of the
+		*  [b,e) point sequence. It is used to early
+		*  prune traversals that do not generate nearest
+		*  neighbors.
+		* \param[in] query_point the query point
+		* \param[in,out] neighbors the computed nearest neighbors
+		*/
+		void get_nearest_neighbors_recursive(
+			index_t node_index, index_t b, index_t e,
+			double* bbox_min, double* bbox_max,
+			double bbox_dist, const double* query_point,
+			NearestNeighbors& neighbors
+			) const;
 
 	protected:
 		std::vector<index_t> point_index_;
