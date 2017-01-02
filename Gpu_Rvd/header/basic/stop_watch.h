5974902fa226bfe2ca3e805933e64b5d033bbe71
JWhan <stormhan1205@gmail.com>
2017-01-02 22:30:15 +0800

    add systemStopwatch

--- Gpu_Rvd/header/basic/stop_watch.h
+++ Gpu_Rvd/header/basic/stop_watch.h
@@ -12,6 +12,68 @@ to record the tatol time in Cpu + Gpu
 
 namespace Gpu_Rvd{
 
+	/**
+	* \brief Measures the time taken by an algorithm.
+	* \details
+	* SystemStopwatch provides functions to get or print the time
+	* elapsed since its construction. The times computed by
+	* SystemStopwatch are expressed as system ticks, which is a system
+	* dependant unit. SystemStopwatch prints three different times:
+	*
+	* - real time: the really elapsed time (depends on the load of the
+	*   machine, i.e. on the others programs that are executed at the
+	*   same time).
+	* - system time: the time spent in system calls.
+	* - user time: the time really spent in the process.
+	*
+	* Example:
+	* \code
+	* {
+	*     SystemStopwatch clock ;
+	*     do_something() ;
+	*     clock.print_elapsed_time(std::cout) ;
+	* }
+	* \endcode
+	*
+	*/
+
+	class  SystemStopwatch {
+	public:
+		/**
+		* \brief SystemStopwatch constructor
+		* \details It remembers the current time as the reference time
+		* for functions elapsed_user_time() and print_elapsed_time().
+		*/
+		SystemStopwatch();
+
+		/**
+		* \brief Prints elapsed time to a stream
+		* \details Prints real, user and system times since the
+		* construction of this SystemStopWatch (in seconds).
+		*/
+		void print_elapsed_time(std::ostream& os) const;
+
+		/**
+		* \brief Get the user elapsed time
+		* \details Returns the user time elapsed since the SystemStopWatch
+		* construction (in seconds)
+		*/
+		double elapsed_user_time() const;
+
+		/**
+		* \details Gets the current time (in seconds).
+		*/
+		static double now();
+
+	private:
+#ifdef GEO_OS_WINDOWS
+		long start_;
+#else
+		tms start_;
+		clock_t start_user_;
+#endif
+	};
+
 	class StopWatch{
 	public:
 
