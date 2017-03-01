f2d43a25608e1438336688c45977626a9b8f302e
JWhan <stormhan1205@gmail.com>
2017-03-01 22:38:19 +0800

    some changes on main function for testing

--- Gpu_Rvd/compute_Rvd/main.cpp
+++ Gpu_Rvd/compute_Rvd/main.cpp
@@ -20,62 +20,61 @@
 int main(int argc, char** argv){
 	using namespace Gpu_Rvd;
 	Process::initialize();
-
+	
 	srand((unsigned int)time(0));
-	const index_t iteration = 1;
-
 	StopWatch S("total task");
-	std::vector<std::string> filenames;
-	if (!Cmd::parse_argu(argc, argv, filenames)){
-		fprintf(stderr, "failed to parse the argument!");
-		return 1;
+	for (int iter = 0; iter < 1; ++iter){
+		const index_t iteration = 100;
+		std::vector<std::string> filenames;
+		if (!Cmd::parse_argu(argc, argv, filenames)){
+			fprintf(stderr, "failed to parse the argument!");
+			return 1;
+		}
+
+		std::string mesh_filename = filenames[0];
+		std::string points_filename = filenames[0];
+		std::string output_filename;
+		if (filenames.size() >= 2) {
+			points_filename = filenames[1];
+		}
+		output_filename = (filenames.size() == 3) ? filenames[2] : "C:\\Users\\JWhan\\Desktop\\DATA\\out.eobj";
+
+		Mesh M_in;
+		Points Points_in;
+
+		if (!mesh_load_obj(mesh_filename, M_in)){
+			fprintf(stderr, "cannot load the mesh from the %s path", mesh_filename);
+			return 1;
+		}
+		points_filename = "C:\\Users\\JWhan\\Desktop\\DATA\\before.xyz";
+		//points_filename = "D:\\Project\\Models\\S2.obj";
+		std::vector<int> sample_facet;
+
+		/*if (!points_load_obj(points_filename, Points_in)){
+			fprintf(stderr, "cannot load the points from the %s path", points_filename);
+			return 1;
+			}*/
+
+		if (!points_load_xyz(points_filename, Points_in, sample_facet)){
+			fprintf(stderr, "cannot load the points from the %s path", points_filename);
+			return 1;
+			}
+		//sample_facet.resize(Points_in.get_vertex_nb());
+		//M_in.init_samples(Points_in, 2000, sample_facet);
+
+		/*if (!points_save_xyz(output_filename, Points_in, sample_facet)){
+			std::cerr << "cannot save the points data" << std::endl;
+			return 1;
+		}*/
+		/*if (!points_save(output_filename, Points_in)){
+			std::cerr << "cannot save the points data" << std::endl;
+			return 1;
+			}*/
+
+		//default settings: k = 20, store_mode
+		CudaRestrictedVoronoiDiagram rvd(&M_in, &Points_in, iteration, sample_facet);
+		rvd.compute_Rvd();
 	}
-
-	std::string mesh_filename = filenames[0];
-	std::string points_filename = filenames[0];
-	std::string output_filename;
-	if (filenames.size() >= 2) {
-		points_filename = filenames[1];
-	}
-	output_filename = (filenames.size() == 3) ? filenames[2] : "C:\\Users\\JWhan\\Desktop\\DATA\\out.eobj";
-
-	Mesh M_in;
-	Points Points_in;
-
-	if (!mesh_load_obj(mesh_filename, M_in)){
-		fprintf(stderr, "cannot load the mesh from the %s path", mesh_filename);
-		return 1;
-	}
-	points_filename = "C:\\Users\\JWhan\\Desktop\\DATA\\before.xyz";
-	//points_filename = "D:\\Project\\Models\\S2.obj";
-	std::vector<int> sample_facet;
-
-	/*if (!points_load_obj(points_filename, Points_in)){
-		fprintf(stderr, "cannot load the points from the %s path", points_filename);
-		return 1;
-	}*/
-	
-	if (!points_load_xyz(points_filename, Points_in, sample_facet)){
-		fprintf(stderr, "cannot load the points from the %s path", points_filename);
-		return 1;
-	}
-	//sample_facet.resize(Points_in.get_vertex_nb());
-	//M_in.init_samples(Points_in, 1000, sample_facet);
-	
-	if (!points_save_xyz(output_filename, Points_in, sample_facet)){
-		std::cerr << "cannot save the points data" << std::endl;
-		return 1;
-	}
-	/*if (!points_save(output_filename, Points_in)){
-		std::cerr << "cannot save the points data" << std::endl;
-		return 1;
-	}*/
-	
-	//default settings: k = 20, store_mode
-	CudaRestrictedVoronoiDiagram rvd(&M_in, &Points_in, iteration, sample_facet);
-	rvd.compute_Rvd();
-
-
 	S.print_elaspsed_time(std::cout);
 	Process::terminate();
 	return 0;
